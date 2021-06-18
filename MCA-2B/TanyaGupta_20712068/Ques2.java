d31a7db366445a0d55bbbbae775de8da14d5997f
TanyaGup <85734415+TanyaGup@users.noreply.github.com>
2021-06-18 13:43:02 +0530

    Add files via upload

---
+++ MCA-2B/TanyaGupta_20712068/Ques2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Ques2
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter 3 Integers :");
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int n3=sc.nextInt();
+		int sum=n1+n2+n3;
+		System.out.println("SUM IS : "+sum);;
+		
+	}
+}
